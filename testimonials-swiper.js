eab9e947bd512bcdcfedc855343fbc03f902372a
Aria <36065920+Fatin-Almalik@users.noreply.github.com>
2022-12-25 13:31:25 +0300

    Update testimonials-swiper.js

--- testimonials-swiper.js
+++ testimonials-swiper.js
@@ -1,5 +1,32 @@
 const swiper = new Swiper('.swiper', {
-  speed: 400,
-  spaceBetween: 100,
-  slidesPerView: 3,
+    speed: 1000,
+    loop: true,
+    autoHeight: false,
+    centeredSlides: true,
+    followFinger: true,
+    freeMode: false,
+    slideToClickedSlide: true,
+    slidesPerView: "auto",
+    slidesPerView: 1.7,
+    spaceBetween: "11%",
+    rewind: false,
+    mousewheel: {
+      forceToAxis: true
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true
+    },
+    breakpoints: {
+      // small screens
+      320: {
+        slidesPerView: 1.2,
+        spaceBetween: "11%"
+      },
+      // larger screens
+      768: {
+        slidesPerView: 1.7,
+        spaceBetween: "11%"
+      },
+    },
 });
